7fb2a12abcb8433be11acfa715eccdadf682c1db
new_commander <new_commander@163.com>
2017-04-11 18:39:19 +0800

    come on, go on...

--- lab/common.h
+++ lab/common.h
@@ -1,1 +1,48 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+struct Task;
+
+struct Node {
+    std::string name;
+    unsigned int tag;
+
+    unsigned int task_tag;
+    struct Task *task;
+
+    std::vector<unsigned int> co_nodes_tag;
+    std::vector<struct Node*> co_nodes;
+};
+
+#define TASK_STATE_INIT 0
+#define TASK_STATE_RUN 1
+#define TASK_STATE_DONE 2
+
+struct Task {
+    std::string name;
+    unsigned int tag;
+    int state;
+
+    unsigned int node_tag;
+    struct Node *node;
+
+    unsigned int parent_task_tag;
+    struct Task *parent_task;
+
+    std::vector<unsigned int> sub_tasks_tag;
+    std::vector<struct Task*> sub_tasks;
+
+    void (*task_init)(void *arg);
+    void (*task_run)(void *arg);
+    void (*task_done)(void *arg);
+};
+
+struct Task_info {
+    std::string name;
+    unsigned int tag;
+    unsigned int node_tag;
+    void (*task_init)(void *arg);
+    void (*task_run)(void *arg);
+    void (*task_done)(void *arg);
+    size_t sub_task_num;
+    unsigned int sub_tasks_tag[];
+};
--- lab/main.cpp
+++ lab/main.cpp
@@ -8,185 +8,162 @@
 
 #include "common.h"
 
-#define TASK_STATE_INIT 0
-#define TASK_STATE_RUN 1
-#define TASK_STATE_DONE 2
-
-struct cc_task {
-	const char *name;
-	int state;
-
-	struct cc_task *parent_task;
-	std::vector<struct cc_task*> sub_tasks;
-
-	void (*task_init)(void *arg);
-	void (*task_run)(void *arg);
-	void (*task_done)(void *arg);
-};
-
-struct cc_task_info {
-	std::string name;
-	void (*task_init)(void *arg);
-	void (*task_run)(void *arg);
-	void (*task_done)(void *arg);
-};
-
 void main_task_init(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_INIT;
-	printf("[%s] init.\n", task->name);
+    task->state = TASK_STATE_INIT;
+    printf("[%s] init.\n", task->name.c_str());
 }
 
 void main_task_run(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
-	unsigned int i;
+    struct Task *task = (struct Task*)arg;
+    size_t i;
 
-	task->state = TASK_STATE_RUN;
-	printf("[%s] run.\n", task->name);
+    task->state = TASK_STATE_RUN;
+    printf("[%s] run.\n", task->name.c_str());
 
-	for (i = 0; i < task->sub_tasks.size(); i++) {
-		if (task->sub_tasks[i]->task_init)
-			task->sub_tasks[i]->task_init(task->sub_tasks[i]);
-		if (task->sub_tasks[i]->task_run)
-			task->sub_tasks[i]->task_run(task->sub_tasks[i]);
-		if (task->sub_tasks[i]->task_done)
-			task->sub_tasks[i]->task_done(task->sub_tasks[i]);
-	}
+    for (i = 0; i < task->sub_tasks.size(); i++) {
+        if (task->sub_tasks[i]->task_init)
+            task->sub_tasks[i]->task_init(task->sub_tasks[i]);
+        if (task->sub_tasks[i]->task_run)
+            task->sub_tasks[i]->task_run(task->sub_tasks[i]);
+        if (task->sub_tasks[i]->task_done)
+            task->sub_tasks[i]->task_done(task->sub_tasks[i]);
+    }
 }
 
 void main_task_done(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_DONE;
-	printf("[%s] done.\n", task->name);
+    task->state = TASK_STATE_DONE;
+    printf("[%s] done.\n", task->name.c_str());
 }
 
 void sub_task_1_init(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_INIT;
-	printf("[%s] init.\n", task->name);
+    task->state = TASK_STATE_INIT;
+    printf("[%s] init.\n", task->name.c_str());
 }
 
 void sub_task_1_run(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_RUN;
-	printf("[%s] run.\n", task->name);
+    task->state = TASK_STATE_RUN;
+    printf("[%s] run.\n", task->name.c_str());
 }
 
 void sub_task_1_done(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_DONE;
-	printf("[%s] done.\n", task->name);
+    task->state = TASK_STATE_DONE;
+    printf("[%s] done.\n", task->name.c_str());
 }
 
 void sub_task_2_init(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_INIT;
-	printf("[%s] init.\n", task->name);
+    task->state = TASK_STATE_INIT;
+    printf("[%s] init.\n", task->name.c_str());
 }
 
 void sub_task_2_run(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_RUN;
-	printf("[%s] run.\n", task->name);
+    task->state = TASK_STATE_RUN;
+    printf("[%s] run.\n", task->name.c_str());
 }
 
 void sub_task_2_done(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_DONE;
-	printf("[%s] done.\n", task->name);
+    task->state = TASK_STATE_DONE;
+    printf("[%s] done.\n", task->name.c_str());
 }
 
 void sub_task_3_init(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_INIT;
-	printf("[%s] init.\n", task->name);
+    task->state = TASK_STATE_INIT;
+    printf("[%s] init.\n", task->name.c_str());
 }
 
 void sub_task_3_run(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_RUN;
-	printf("[%s] run.\n", task->name);
+    task->state = TASK_STATE_RUN;
+    printf("[%s] run.\n", task->name.c_str());
 }
 
 void sub_task_3_done(void *arg)
 {
-	struct cc_task *task = (struct cc_task*)arg;
+    struct Task *task = (struct Task*)arg;
 
-	task->state = TASK_STATE_DONE;
-	printf("[%s] done.\n", task->name);
+    task->state = TASK_STATE_DONE;
+    printf("[%s] done.\n", task->name.c_str());
 }
 
-void cc_task_config(struct cc_task *task, struct cc_task *parent_task, struct cc_task_info task_info)
+void task_config(struct Task *task, struct Task *parent_task, struct Task_info task_info)
 {
-	task->name = task_info.name.c_str();
-	task->state = 0;
-	task->parent_task = parent_task;
-	task->sub_tasks.clear();
-	task->task_init = task_info.task_init;
-	task->task_run = task_info.task_run;
-	task->task_done = task_info.task_done;
+    task->name = task_info.name;
+    task->state = 0;
+    task->parent_task = parent_task;
+    task->sub_tasks.clear();
+    task->task_init = task_info.task_init;
+    task->task_run = task_info.task_run;
+    task->task_done = task_info.task_done;
 }
 
 int main(int argc, char **argv)
 {
-	struct cc_task_info info[] = {
-		{ "main task", main_task_init, main_task_run, main_task_done },
-		{ "sub task 1", sub_task_1_init, sub_task_1_run, sub_task_1_done },
-		{ "sub task 2", sub_task_2_init, sub_task_2_run, sub_task_2_done },
-		{ "sub task 3", sub_task_3_init, sub_task_3_run, sub_task_3_done }
-	};
-	struct cc_task main_task, *sub_task = NULL;
-	unsigned int i;
+    struct Task_info info[] = {
+        { "main task", 0, 0xffffffff, main_task_init, main_task_run, main_task_done, 3, 0xfffffffe, 0xfffffffd, 0xfffffffc },
+        { "sub task 1", 0x1, 0xfffffffe, sub_task_1_init, sub_task_1_run, sub_task_1_done, 0 },
+        { "sub task 2", 0x2, 0xfffffffd, sub_task_2_init, sub_task_2_run, sub_task_2_done, 0 },
+        { "sub task 3", 0x3, 0xfffffffc, sub_task_3_init, sub_task_3_run, sub_task_3_done, 0 },
+    };
+    struct Task main_task, *sub_task = NULL;
+    unsigned int i;
 
-	memset(&main_task, 0, sizeof(struct cc_task));
-	cc_task_config(&main_task, NULL, info[0]);
+    memset(&main_task, 0, sizeof(struct Task));
+    task_config(&main_task, NULL, info[0]);
 
-	for (i = 1; i < ARRAY_SIZE(info); i++) {
-		sub_task = (struct cc_task*)calloc(1, sizeof(struct cc_task));
-		if (!sub_task) {
-			printf("[%s]: Allocate memory for #%d sub_task failed.\n", main_task.name, i);
-			for (i = 0; i < main_task.sub_tasks.size(); i++)
-				free(main_task.sub_tasks[i]);
-			main_task.sub_tasks.clear();
-			return -ENOMEM;
-		}
-		cc_task_config(sub_task, &main_task, info[i]);
-		main_task.sub_tasks.push_back(sub_task);
-		sub_task = NULL;
-	}
+    for (i = 1; i < ARRAY_SIZE(info); i++) {
+        sub_task = (struct Task*)calloc(1, sizeof(struct Task));
+        if (!sub_task) {
+            printf("[%s]: Allocate memory for #%d sub_task failed.\n", main_task.name.c_str(), i);
+            for (i = 0; i < main_task.sub_tasks.size(); i++)
+                free(main_task.sub_tasks[i]);
+            main_task.sub_tasks.clear();
+            return -ENOMEM;
+        }
+        task_config(sub_task, &main_task, info[i]);
+        main_task.sub_tasks.push_back(sub_task);
+        sub_task = NULL;
+    }
 
-	if (main_task.task_init)
-		main_task.task_init(&main_task);
-	if (main_task.task_run)
-		main_task.task_run(&main_task);
-	if (main_task.task_done)
-		main_task.task_done(&main_task);
+    if (main_task.task_init)
+        main_task.task_init(&main_task);
+    if (main_task.task_run)
+        main_task.task_run(&main_task);
+    if (main_task.task_done)
+        main_task.task_done(&main_task);
 
-	for (i = 0; i < main_task.sub_tasks.size(); i++)
-		free(main_task.sub_tasks[i]);
+    for (i = 0; i < main_task.sub_tasks.size(); i++)
+        free(main_task.sub_tasks[i]);
 
-	main_task.sub_tasks.clear();
+    main_task.sub_tasks.clear();
 
-	return 0;
+    return 0;
 }
