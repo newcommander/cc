c7650eb4d52a24c55979c558dc2bd5676f5342db
new_commander <new_commander@163.com>
2017-04-12 18:18:29 +0800

    go on...

--- lab/common.h
+++ lab/common.h
@@ -1,29 +1,21 @@
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#include <string>
 
-struct Task;
-struct Action;
+class Task;
+class Action;
 
-struct Node {
+class Node {
+public:
     std::string name;
     unsigned int tag;
 
     unsigned int task_tag;
-    struct Task *task;
+    Task *task;
 
     unsigned int action_tag;
-    struct Action *action;
+    Action *action;
 
-    std::vector<unsigned int> co_nodes_tag;
-    std::vector<struct Node*> co_nodes;
-};
-
-struct Node_info {
-    std::string name;
-    unsigned int tag;
-    unsigned int task_tag;
-    unsigned int action_tag;
-    size_t co_node_num;
-    unsigned int co_nodes[];
+    std::vector<unsigned int> co_nodes_tags;
+    std::vector<Node*> co_nodes;
 };
 
 #define TASK_STATE_IDLE 0
@@ -31,34 +23,24 @@ struct Node_info {
 #define TASK_STATE_RUN 2
 #define TASK_STATE_DONE 3
 
-struct Task {
+class Task {
+public:
     std::string name;
     unsigned int tag;
     int state;
 
     unsigned int node_tag;
-    struct Node *node;
+    Node *node;
 
     unsigned int parent_task_tag;
-    struct Task *parent_task;
+    Task *parent_task;
 
-    std::vector<unsigned int> sub_tasks_tag;
-    std::vector<struct Task*> sub_tasks;
+    std::vector<unsigned int> sub_tasks_tags;
+    std::vector<Task*> sub_tasks;
 
     void (*task_init)(void *arg);
     void (*task_run)(void *arg);
     void (*task_done)(void *arg);
-};
-
-struct Task_info {
-    std::string name;
-    unsigned int tag;
-    unsigned int node_tag;
-    void (*task_init)(void *arg);
-    void (*task_run)(void *arg);
-    void (*task_done)(void *arg);
-    size_t sub_task_num;
-    unsigned int sub_tasks_tag[];
 };
 
 #define ACTION_STATE_IDLE 0
@@ -66,24 +48,16 @@ struct Task_info {
 #define ACTION_STATE_RUN 2
 #define ACTION_STATE_DONE 3
 
-struct Action {
+class Action {
+public:
     std::string name;
     unsigned int tag;
     int state;
 
     unsigned int node_tag;
-    struct Node *node;
+    Node *node;
 
     void (*action_init)(void *arg);
     void (*action_run)(void *arg);
     void (*action_done)(void *arg);
-};
-
-struct Action_info {
-    std::string name;
-    unsigned int tag;
-    unsigned int node_tag;
-    void (*task_init)(void *arg);
-    void (*task_run)(void *arg);
-    void (*task_done)(void *arg);
 };
--- lab/main.cpp
+++ lab/main.cpp
@@ -1,181 +1,107 @@
-#include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-
+#include <json/json.h>
 #include "common.h"
 
-static std::vector<struct Node*> g_nodes;
-static std::vector<struct Task*> g_tasks;
-static std::vector<struct Action*> g_actions;
+static std::vector<Node*> g_nodes;
+static std::vector<Task*> g_tasks;
+static std::vector<Action*> g_actions;
 
-void main_task_init(void *arg)
+int load_node(Json::Value &value)
 {
-    struct Task *task = (struct Task*)arg;
+    Json::Value::ArrayIndex i;
+    Node *node;
 
-    task->state = TASK_STATE_INIT;
-    printf("[%s] init.\n", task->name.c_str());
-}
-
-void main_task_run(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-    size_t i;
-
-    task->state = TASK_STATE_RUN;
-    printf("[%s] run.\n", task->name.c_str());
-
-    for (i = 0; i < task->sub_tasks.size(); i++) {
-        if (task->sub_tasks[i]->task_init)
-            task->sub_tasks[i]->task_init(task->sub_tasks[i]);
-        if (task->sub_tasks[i]->task_run)
-            task->sub_tasks[i]->task_run(task->sub_tasks[i]);
-        if (task->sub_tasks[i]->task_done)
-            task->sub_tasks[i]->task_done(task->sub_tasks[i]);
+    if (!value.isMember("name") ||
+        !value.isMember("tag") ||
+        !value.isMember("task_tag") ||
+        !value.isMember("action_tag") ||
+        !value.isMember("co_nodes")) {
+        std::cout << "Invalid node." << std::endl;
+        return -1;
     }
+
+    node = new Node();
+    if (!node)
+        return -1;
+
+    node->name = value["name"].asString();
+    node->tag = value["tag"].asUInt();
+    node->task_tag = value["task_tag"].asUInt();
+    node->action_tag = value["action_tag"].asUInt();
+
+    for (i = 0; i < value["co_nodes"].size(); i++)
+        node->co_nodes_tags.push_back(value["co_nodes"][i].asUInt());
+
+    g_nodes.push_back(node);
+
+    return 0;
 }
 
-void main_task_done(void *arg)
+int load_task(Json::Value &value)
 {
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_DONE;
-    printf("[%s] done.\n", task->name.c_str());
+    return 0;
 }
 
-void sub_task_1_init(void *arg)
+int load_action(Json::Value &value)
 {
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_INIT;
-    printf("[%s] init.\n", task->name.c_str());
+    return 0;
 }
 
-void sub_task_1_run(void *arg)
+int load_data(char *file)
 {
-    struct Task *task = (struct Task*)arg;
+    Json::Value::ArrayIndex i;
+    Json::Reader reader;
+    Json::Value root;
+    std::filebuf fb;
 
-    task->state = TASK_STATE_RUN;
-    printf("[%s] run.\n", task->name.c_str());
-}
+    if (fb.open(file, std::ios::in)) {
+        std::istream is(&fb);
+        if (!reader.parse(is, root)) {
+            std::cout << "Parse json data failed." << std::endl;
+            fb.close();
+            return -1;
+        }
+        fb.close();
+    } else {
+        std::cout << "Open file "<< file << " failed.\n" << std::endl;
+        return -1;
+    }
 
-void sub_task_1_done(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
+    if (!root.isArray()) {
+        std::cout << file << ": Json data in file must be an Array." << std::endl;
+        return -1;
+    }
 
-    task->state = TASK_STATE_DONE;
-    printf("[%s] done.\n", task->name.c_str());
-}
+    for (i = 0; i < root.size(); i++) {
+        if (root[i].isMember("type")) {
+            if (root[i]["type"] == "NODE") {
+                if (load_node(root[i]) < 0)
+                    std::cout << "load #" << i << " node failed." << std::endl;
+            } else if (root[i]["type"] == "TASK") {
+                if (load_task(root[i]) < 0)
+                    std::cout << "load #" << i << " task failed." << std::endl;
+            } else if (root[i]["type"] == "ACTION") {
+                if (load_action(root[i]) < 0)
+                    std::cout << "load #" << i << " action failed." << std::endl;
+            }
+        }
+    }
 
-void sub_task_2_init(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_INIT;
-    printf("[%s] init.\n", task->name.c_str());
-}
-
-void sub_task_2_run(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_RUN;
-    printf("[%s] run.\n", task->name.c_str());
-}
-
-void sub_task_2_done(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_DONE;
-    printf("[%s] done.\n", task->name.c_str());
-}
-
-void sub_task_3_init(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_INIT;
-    printf("[%s] init.\n", task->name.c_str());
-}
-
-void sub_task_3_run(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_RUN;
-    printf("[%s] run.\n", task->name.c_str());
-}
-
-void sub_task_3_done(void *arg)
-{
-    struct Task *task = (struct Task*)arg;
-
-    task->state = TASK_STATE_DONE;
-    printf("[%s] done.\n", task->name.c_str());
-}
-
-void task_config(struct Task *task, struct Task *parent_task, struct Task_info task_info)
-{
-    unsigned int i;
-
-    task->name = task_info.name;
-    task->tag = task_info.tag;
-    task->state = TASK_STATE_IDLE;
-    task->node_tag = task_info.node_tag;
-    task->node = NULL;
-    if (parent_task)
-        task->parent_task_tag = parent_task->tag;
-    else
-        task->parent_task_tag = 0;
-    task->parent_task = NULL;
-    for (i = 0; i < task_info.sub_task_num; i++)
-        task->sub_tasks_tag.push_back(task_info.sub_tasks_tag[i]);
-    task->sub_tasks.clear();
-    task->task_init = task_info.task_init;
-    task->task_run = task_info.task_run;
-    task->task_done = task_info.task_done;
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    struct Task_info info[] = {
-        { "main task", 0, 0xffffffff, main_task_init, main_task_run, main_task_done, 3, 0xfffffffe, 0xfffffffd, 0xfffffffc },
-        { "sub task 1", 0x1, 0xfffffffe, sub_task_1_init, sub_task_1_run, sub_task_1_done, 0 },
-        { "sub task 2", 0x2, 0xfffffffd, sub_task_2_init, sub_task_2_run, sub_task_2_done, 0 },
-        { "sub task 3", 0x3, 0xfffffffc, sub_task_3_init, sub_task_3_run, sub_task_3_done, 0 },
-    };
-    struct Task *task = NULL;
-    unsigned int i;
-
-    for (i = 0; i < ARRAY_SIZE(info); i++) {
-        task = (struct Task*)calloc(1, sizeof(struct Task));
-        if (!task) {
-            printf("Allocate memory for #%d task failed.\n", i);
-            for (i = 0; i < main_task.sub_tasks.size(); i++)
-                free(main_task.sub_tasks[i]);
-            main_task.sub_tasks.clear();
-            return -ENOMEM;
-        }
-        task_config(task, &main_task, info[i]);
-        g_tasks.push_back(task);
-        task = NULL;
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <data_file>" << std::endl;
+        return 1;
     }
 
-    if (main_task.task_init)
-        main_task.task_init(&main_task);
-    if (main_task.task_run)
-        main_task.task_run(&main_task);
-    if (main_task.task_done)
-        main_task.task_done(&main_task);
-
-    for (i = 0; i < main_task.sub_tasks.size(); i++)
-        free(main_task.sub_tasks[i]);
-
-    main_task.sub_tasks.clear();
+    if (load_data(argv[1]) < 0)
+        return 1;
 
     return 0;
 }
